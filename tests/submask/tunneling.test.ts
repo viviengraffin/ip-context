4604b8e85e7e0f1345749c036bba1264ce7f0f06
Vivien Graffin <graffinvivien@gmail.com>
2025-10-08 15:39:46 +0200

    Add unit tests for tunneling conversions

---
+++ tests/submask/tunneling.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from "vitest";
+import { IPv4Address, IPv6Address } from "../../src/libs/ipaddress.ts";
+import { TUNNELING_MODES } from "../../src/libs/tunneling.ts";
+
+describe("Mapped", () => {
+  test("IPv6Address.toIPv4Address", () => {
+    expect(
+      IPv6Address.fromIPv4MappedString("::ffff:192.168.0.1")
+        .toIPv4Address(TUNNELING_MODES.MAPPED)
+        .equals(new IPv4Address([192, 168, 0, 1])),
+    ).toBe(true);
+  });
+
+  test("IPv4Address.toIPv6Address", () => {
+    expect(
+      IPv4Address.fromString("172.10.5.10")
+        .toIPv6Address(TUNNELING_MODES.MAPPED)
+        .equals(new IPv6Address([0, 0, 0, 0, 0, 0xFFFF, 0xAC0A, 0x050A])),
+    ).toBe(true);
+  });
+});
+
+describe("6to4", () => {
+  test("IPv6Address.toIPv4Address", () => {
+    expect(
+      IPv6Address.fromString("2002:c0a8:1::")
+        .toIPv4Address(TUNNELING_MODES.SIX_TO_FOUR)
+        .equals(new IPv4Address([192, 168, 0, 1])),
+    ).toBe(true);
+  });
+
+  test("IPv4Address.toIPv6Address", () => {
+    expect(
+      IPv4Address.fromString("172.10.5.10")
+        .toIPv6Address(TUNNELING_MODES.SIX_TO_FOUR)
+        .equals(new IPv6Address([0x2002, 0xAC0A, 0x050A, 0, 0, 0, 0, 0])),
+    ).toBe(true);
+  });
+});
